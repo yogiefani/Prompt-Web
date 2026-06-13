a350292e77268f3489a9d417530800f511ab1104
yogiefani <yogiefaniyc@gmail.com>
2026-06-13 23:32:51 +0700

    chore: refine ts definition

--- src/types/phantom.d.ts
+++ src/types/phantom.d.ts
@@ -1,24 +1,30 @@
-import * as React from "react";
+type PhantomUIProps = import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLElement> & {
+  loading?: boolean | string;
+  animation?: string;
+  "shimmer-direction"?: string;
+  "shimmer-color"?: string;
+  "background-color"?: string;
+  duration?: number | string;
+  stagger?: number | string;
+  reveal?: number | string;
+  count?: number | string;
+  "count-gap"?: number | string;
+  "fallback-radius"?: number | string;
+  debug?: boolean | string;
+  "loading-label"?: string;
+  "pierce-shadow"?: boolean | string;
+}, HTMLElement>;
 
-declare global {
+declare namespace JSX {
+  interface IntrinsicElements {
+    "phantom-ui": PhantomUIProps;
+  }
+}
+
+declare module "react" {
   namespace JSX {
     interface IntrinsicElements {
-      "phantom-ui": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
-        loading?: boolean | string;
-        animation?: string;
-        "shimmer-direction"?: string;
-        "shimmer-color"?: string;
-        "background-color"?: string;
-        duration?: number | string;
-        stagger?: number | string;
-        reveal?: number | string;
-        count?: number | string;
-        "count-gap"?: number | string;
-        "fallback-radius"?: number | string;
-        debug?: boolean | string;
-        "loading-label"?: string;
-        "pierce-shadow"?: boolean | string;
-      }, HTMLElement>;
+      "phantom-ui": PhantomUIProps;
     }
   }
 }
