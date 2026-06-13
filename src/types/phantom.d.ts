823089bedc40e7912a0bed4fb06ef5b042a30ff0
yogiefani <yogiefaniyc@gmail.com>
2026-06-13 23:29:21 +0700

    fix: ts declaration for phantom-ui

--- src/types/phantom.d.ts
+++ src/types/phantom.d.ts
@@ -1,20 +1,24 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    "phantom-ui": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
-      loading?: boolean | string;
-      animation?: string;
-      "shimmer-direction"?: string;
-      "shimmer-color"?: string;
-      "background-color"?: string;
-      duration?: number | string;
-      stagger?: number | string;
-      reveal?: number | string;
-      count?: number | string;
-      "count-gap"?: number | string;
-      "fallback-radius"?: number | string;
-      debug?: boolean | string;
-      "loading-label"?: string;
-      "pierce-shadow"?: boolean | string;
-    }, HTMLElement>;
+import * as React from "react";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "phantom-ui": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+        loading?: boolean | string;
+        animation?: string;
+        "shimmer-direction"?: string;
+        "shimmer-color"?: string;
+        "background-color"?: string;
+        duration?: number | string;
+        stagger?: number | string;
+        reveal?: number | string;
+        count?: number | string;
+        "count-gap"?: number | string;
+        "fallback-radius"?: number | string;
+        debug?: boolean | string;
+        "loading-label"?: string;
+        "pierce-shadow"?: boolean | string;
+      }, HTMLElement>;
+    }
   }
 }
